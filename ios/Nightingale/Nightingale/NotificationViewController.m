a12f6bab3db75ef458d0174eb1b274fa4a43f030
Francesco Mattia <francesco.mattia@akqa.com>
2014-02-26 16:48:47 +0000

    notification screen

---
+++ ios/Nightingale/Nightingale/NotificationViewController.m
@@ -0,0 +1,45 @@
+//
+//  NotificationViewController.m
+//  Nightingale
+//
+//  Created by Francesco Mattia on 26/02/2014.
+//  Copyright (c) 2014 AKQA. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+
+@interface NotificationViewController ()
+
+@end
+
+@implementation NotificationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)closeButtonPressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)startButtonPressed:(id)sender {
+}
+
+@end
