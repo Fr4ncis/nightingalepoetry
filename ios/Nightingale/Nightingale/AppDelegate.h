a3685c6d1433e516802c26b2dc3b739ab5b81cf1
Francesco Mattia <francesco.mattia@akqa.com>
2014-02-26 14:02:44 +0000

    first commit ios app

---
+++ ios/Nightingale/Nightingale/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nightingale
+//
+//  Created by Francesco Mattia on 26/02/2014.
+//  Copyright (c) 2014 AKQA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
